c75a4ff2fcee330bdc8cad9595f85600074c68f6
roman.kuznetsov <roman.kuznetsov@g5e.com>
2022-06-30 17:47:52 +0300

    Added admin messages anf buttons

--- src/main/java/prizedrowtelegrambot/enums/ButtonNameEnum.java
+++ src/main/java/prizedrowtelegrambot/enums/ButtonNameEnum.java
@@ -4,7 +4,15 @@ public enum ButtonNameEnum {
     REGISTER("Зробити донат та зареєструватися у розіграші"),
     ACCEPT_PAYMENT("Approve Payment"),
     DECLINE_PAYMENT("Decline Payment"),
-    USER_PAYMENT_CONFIRMATION("Я підтверджую що зробив донат! Піревірте будьласка!");
+    USER_PAYMENT_CONFIRMATION("Я підтверджую що зробив донат! Піревірте будьласка!"),
+
+    SEND_15_MIN_REMINDER("Send 15 min reminder"),
+
+    START_DRAW("Start draw"),
+
+    GET_CONFIRMED_USERS_LIST("Get confirmed users");
+
+
 
     private final String buttonName;
 
